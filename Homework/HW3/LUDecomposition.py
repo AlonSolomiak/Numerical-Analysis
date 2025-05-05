6447f39d395dab151190b4fd613a71f09d5949de
Alon Solomiak <109109875+AlonSolomiak@users.noreply.github.com>
2025-05-05 22:45:41 +0300

    Fixed Ofry's mistakes

--- Homework/HW3/LUDecomposition.py
+++ Homework/HW3/LUDecomposition.py
@@ -107,8 +107,8 @@ def MatrixVectorMultiply(A: np.ndarray, v: np.ndarray) -> np.ndarray:
     result = np.array([0]*A.shape[1], dtype=float)
     
     for matrix_row in range(A.shape[0]):
-        for matrix_row in range(A.shape[1]):
-            result[matrix_col] += A[matrix_row,matrix_col] * v[matrix_row]
+        for matrix_col in range(A.shape[1]):
+            result[matrix_col] += A[matrix_row,matrix_col] * v[matrix_col]
     
     return result
 
