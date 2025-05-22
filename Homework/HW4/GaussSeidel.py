f33c80cf56bb6a53032da51fbd276adc6da899cd
Alony203 <tkuiso203@gmail.com>
2025-05-22 16:15:21 +0300

    HW4Q34 initial answer

---
+++ Homework/HW4/GaussSeidel.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def GaussSeidel(A: np.ndarray, b: np.ndarray, m: int) -> np.ndarray:
+    n = len(A)
+    x_i = np.array([0] * n, dtype=float)
+    x_f = np.array([0] * n, dtype=float)
+    
+    while convergence_criterion(x_i, x_f, m):
+        print(x_i)
+        for i in range(n):
+            x_i[i] = x_f[i]
+        for i in range(n):
+            sum1 = 0
+            for j in range(i):
+                sum1 += A[i][j] * x_f[j]
+            sum2 = 0
+            for j in range(i+1, n):
+                sum2 += A[i][j] * x_i[j]
+
+            x_f[i] = (b[i] - sum1 - sum2) / A[i][i]
+
+    return x_f
+        
+
+def convergence_criterion(x_i: np.ndarray, x_f: np.ndarray, m: int) -> bool:
+    max_diff = max(abs(x_f[i] - x_i[i]) for i in range(1, len(x_i)))
+    print(max_diff)
+    return max_diff > 10**-m
