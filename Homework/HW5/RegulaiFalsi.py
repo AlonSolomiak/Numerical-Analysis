4ba3f9771d082da5c0b5bc24db7ef74560a170d3
Alony203 <tkuiso203@gmail.com>
2025-05-29 05:01:47 +0300

    added HW5

---
+++ Homework/HW5/RegulaiFalsi.py
@@ -0,0 +1,17 @@
+def RegulaiFalsi(XL, XR, n):
+    x = XL - (f(XL) * (XR - XL)) / (f(XR) - f(XL))
+    i = 0
+
+    while abs(f(x)) > 10**-n:
+        if f(XL) * f(x) < 0:
+            XR = x
+        else:
+            XL = x
+        x_new = XL - (f(XL) * (XR - XL)) / (f(XR) - f(XL))
+        i += 1
+        if abs(x_new - x) < 10**-n:
+            break
+        x = x_new
+
+
+    return x, i
